77899849e8055c25ad8d9b1ee1c325669e1ea647
Gurjas Singh <gurjassingh92@gmail.com>
2021-02-13 17:18:47 +0530

    post method with sending details to html page

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -9,6 +9,21 @@ def index():
 @app.route("/user")
 def user_details():
     return render_template('index.html')
+
+@app.route("/details", methods=['GET', 'POST'])
+def details():
+    if request.method == 'POST': ## post method
+        data = request.form
+        username = data['username']
+        # print("username given to the user was", username)
+        # return "POST method was called"
+        # return "username given to the user was {}".format(username)
+        data = {
+            'username': username
+        }
+        return render_template('details.html', data = data)
+    else: ## get method
+        return render_template('details.html')
 
 if __name__ == "__main__":
     app.run()
