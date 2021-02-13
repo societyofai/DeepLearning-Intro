b5d4702224552761b95e775989ffda44ef42d1c3
Gurjas Singh <gurjassingh92@gmail.com>
2021-02-13 17:05:16 +0530

    added new index.html template and route

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -6,9 +6,9 @@ app = Flask(__name__)
 def index():
     return "Hello World"
 
-@app.route("/hi") ## facebook.com/messages.
-def hi_route():
-    return "Hi user"
+@app.route("/user")
+def user_details():
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run()
