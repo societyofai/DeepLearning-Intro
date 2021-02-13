651a91bff72ddc35945995ef70bc539770b1e89c
ŚÏĐĐHÃŘŤH <siddharthtewani@gmail.com>
2021-02-13 17:41:44 +0530

    added another route

--- main.py
+++ main.py
@@ -10,20 +10,19 @@ def index():
 def user_details():
     return render_template('index.html')
 
+
 @app.route("/details", methods=['GET', 'POST'])
 def details():
     if request.method == 'POST': ## post method
         data = request.form
         username = data['username']
-        # print("username given to the user was", username)
-        # return "POST method was called"
-        # return "username given to the user was {}".format(username)
+        
         data = {
             'username': username
         }
-        return render_template('details.html', data = data)
+        return render_template('list_output.html', data = data)
     else: ## get method
-        return render_template('details.html')
+        return render_template('list_output.html')
 
 if __name__ == "__main__":
     app.run()
