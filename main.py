4d04db1d8016b258b401c9e85c82425a74488e0c
ŚÏĐĐHÃŘŤH <siddharthtewani@gmail.com>
2021-02-14 16:45:47 +0530

    Added a new

--- main.py
+++ main.py
@@ -20,9 +20,27 @@ def details():
         data = {
             'username': username
         }
+        return render_template('details.html', data = data)
+    else: ## get method
+        return render_template('details.html')
+
+
+
+@app.route("/detail_of_user", methods=['GET', 'POST'])
+def detail_of_user():
+    if request.method == 'POST': 
+        data = request.form
+        username = data['username']
+        class_user=data['class_user']        
+        data = {
+            'username': username,
+            'class': class_user
+        }
         return render_template('list_output.html', data = data)
     else: ## get method
         return render_template('list_output.html')
+
+
 
 if __name__ == "__main__":
     app.run()
